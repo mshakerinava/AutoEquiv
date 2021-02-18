faea5e52b92c26a4c04b70f0b6c230fc3fd4fc58
Mehran Shakerinava <mehranshakerinava@gmail.com>
2021-02-18 10:47:03 +0330

    add utils.py and move some imports inside functions

--- autoequiv/viz.py
+++ autoequiv/viz.py
@@ -1,10 +1,12 @@
 from autoequiv.core import *
-import numpy as np
-import networkx as nx
-import matplotlib.pyplot as plt
+from autoequiv.utils import *
 
 
-def draw_colored_bipartite_graph(n, m, colors, edge_width=2.0):
+def draw_colored_bipartite_graph(colors, node_size=5, edge_width=2):
+    import matplotlib.pyplot as plt
+    import networkx as nx
+    w = dict_to_arr(colors)
+    n, m = w.shape
     G = nx.Graph()
     for i in range(n):
         G.add_node('i%d' % i, pos=(i - (n - 1) / 2, +1))
@@ -12,45 +14,39 @@ def draw_colored_bipartite_graph(n, m, colors, edge_width=2.0):
         G.add_node('o%d' % i, pos=(i - (m - 1) / 2, -1))
     for i in range(n):
         for j in range(m):
-            G.add_edge('i%d' % i, 'o%d' % j, c=colors[(i, j)])
-    fig = plt.figure(figsize=(11, 4))
+            G.add_edge('i%d' % i, 'o%d' % j, c=w[i, j])
     pos = nx.get_node_attributes(G, 'pos')
     c_dict = nx.get_edge_attributes(G, 'c')
     c = [c_dict[x] for x in G.edges]
-    nx.draw_networkx_edges(G, pos=pos, width=edge_width, edge_color=c, edge_cmap=plt.cm.viridis, alpha=0.6)
-    nx.draw_networkx_nodes(G, pos=pos, node_color='k', node_size=5)
+    nx.draw_networkx_edges(G, pos=pos, width=edge_width, edge_color=c, edge_cmap=plt.cm.rainbow, alpha=0.6)
+    nx.draw_networkx_nodes(G, pos=pos, node_color='k', node_size=node_size)
     plt.axis('off')
-    fig.savefig('bipartite_graph_plot.svg', bbox_inches='tight')
-    fig.savefig('bipartite_graph_plot.pdf', bbox_inches='tight')
-    fig.savefig('bipartite_graph_plot.png', bbox_inches='tight', dpi=600)
-    plt.show()
-    plt.close()
 
 
-def draw_colored_matrix(n, m, colors):
-    x = np.zeros((n, m), dtype=int)
-    for i in range(n):
-        for j in range(m):
-            x[i, j] = colors.get((i, j), -1)
-    ax = plt.matshow(x, cmap='jet')
-    fig = ax.figure
-    plt.axis('off')
-    fig.savefig('matrix_plot.svg', bbox_inches='tight')
-    fig.savefig('matrix_plot.pdf', bbox_inches='tight')
-    fig.savefig('matrix_plot.png', bbox_inches='tight', dpi=600)
-    plt.show()
-    plt.close()
-
-
-def draw_colored_vector(n, colors):
-    x = np.zeros((1, n), dtype=int)
-    for i in range(n):
-        x[0, i] = colors.get(i, -1)
-    ax = plt.matshow(x, cmap='jet')
-    fig = ax.figure
-    plt.axis('off')
-    fig.savefig('vector_plot.svg', bbox_inches='tight')
-    fig.savefig('vector_plot.pdf', bbox_inches='tight')
-    fig.savefig('vector_plot.png', bbox_inches='tight', dpi=600)
-    plt.show()
-    plt.close()
+# adapted from code provided by Siamak Ravanbakhsh
+def draw_colored_matrix(colors, cmap='rainbow', markersize=20):
+    import numpy as np
+    import matplotlib
+    import matplotlib.pyplot as plt
+    import matplotlib.cm as cmx
+    import pylab as mpl
+    mrklist = ['3', 'd', ',', '2', '8', 's', '|', '^', '<', '4', 'H', '+', 'o', 'v', 'p', 'D', '>', '1', '.', '_', '*', 'h']
+    w = dict_to_arr(colors)
+    wvals = np.unique(w.ravel()).astype(int)
+    cm = plt.get_cmap('Greys') 
+    cNorm  = matplotlib.colors.Normalize(vmin=0, vmax=len(wvals) - 1)
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
+    if markersize > 0:
+        for v, wval in enumerate(wvals):
+            x, y = np.nonzero(w == wval)
+            x = w.shape[0] - x - 1
+            plt.scatter(x, y, s=markersize, marker=mrklist[wval % len(mrklist)], color=scalarMap.to_rgba(wval), alpha=1)
+    plt.xticks(np.arange(w.shape[0]-1)+.5, [])
+    plt.yticks(np.arange(w.shape[1]-1)+.5, [])
+    plt.xlim(-.5, w.shape[0]-.5)
+    plt.ylim(-.5, w.shape[1]-.5)
+    plt.grid(b=True, which='major', color='grey', linestyle='-', alpha=.5)
+    mpl.imshow(w[::-1, :].T, cmap=cmap, alpha=.8, vmin=0, vmax=len(wvals) - 1)
+    ax = plt.gca()
+    ax.xaxis.set_ticks_position('none')
+    ax.yaxis.set_ticks_position('none')
